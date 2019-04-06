96d36b2449387579409c2be2daba0e16873443e9
Łukasz Godziejewski <lgodziejewski@gmail.com>
2019-04-06 17:43:19 +0200

    [STAGE-2] Configure database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CalendarModule } from './calendar/calendar.module';
 
 @Module({
-  imports: [CalendarModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'calendar',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    CalendarModule,
+  ],
 })
 export class AppModule {}
